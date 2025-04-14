60d2c0397187ed053fe1ca8224020141553ca965
raimauricio <rai_mauricio@hotmail.com>
2025-04-13 23:30:27 -0300

    Atualizando header com componentes do prime

--- mens-shop/src/app/components/header/header.component.ts
+++ mens-shop/src/app/components/header/header.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { BadgeModule } from 'primeng/badge';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  itensCarrinho = null;
   isLoggedIn = false;
 
   toggleLogin() {
